33b9c77a478e0e7dcecf8acdc4ad84f9bff29e98
2109231Frank <2109231@students.wits.ac.za>
2021-05-21 15:17:52 +0200

    finished nav animations

--- site.js
+++ site.js
@@ -1,9 +1,30 @@
+
 const navSlideIn = () => {
     const threestripe = document.querySelector('.threestripe');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
 
     threestripe.addEventListener('click', () => {
+
+        //Navigation side bar activation.
         nav.classList.toggle('nav-active');
-    })
+
+        //Link animation.
+        navLinks.forEach((link, index) => {
+        
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkAppear 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+                
+            });
+        
+        //Three lines animation.
+        threestripe.classList.toggle('toggle');
+    });
 }
+
 navSlideIn();
