b8a716cc557dc4294e255fb29cd05ea526aee8a0
2109231Frank <2109231@students.wits.ac.za>
2021-05-26 20:51:54 +0200

    background animation

--- site.js
+++ site.js
@@ -28,3 +28,25 @@ const navSlideIn = () => {
 }
 
 navSlideIn();
+
+//Floaty square animation.
+function squeare_spawn(){
+    const section = document.querySelector('section');
+    const square = document.createElement('span');
+
+    var size = Math.random()*50;
+
+    square.style.width = 20 + size + 'px';
+    square.style.height = 20 + size + 'px';
+
+    square.style.top = Math.random() * innerHeight + 'px';
+    square.style.left = Math.random() * innerWidth + 'px';
+
+    section.appendChild(square);
+
+    setTimeout(() => {
+        square.remove()
+    }, 5000)
+}
+
+setInterval(squeare_spawn, 150)
