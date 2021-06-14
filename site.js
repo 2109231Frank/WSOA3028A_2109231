61d69002654d084be97825974c0c3df98134a16d
2109231Frank <2109231@students.wits.ac.za>
2021-06-14 17:22:02 +0200

    started Java

--- site.js
+++ site.js
@@ -1,32 +1,5 @@
 
-const navSlideIn = () => {
-    const threestripe = document.querySelector('.threestripe');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const navigationLinks = document.querySelector(".nav a")
 
 
-    threestripe.addEventListener('click', () => {
-
-        //Navigation side bar activation.
-        nav.classList.toggle('nav-active');
-
-        //Link animation.
-        navLinks.forEach((link, index) => {
-        
-            if (link.style.animation) {
-                link.style.animation = '';
-            }
-            else {
-                link.style.animation = `navLinkAppear 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-                
-            });
-        
-        //Three lines animation.
-        threestripe.classList.toggle('toggle');
-    });
-}
-
-navSlideIn();
-
-
+navigationLinks.forEach(elem => elem.addEventListener("click", navigationLinkClick));
