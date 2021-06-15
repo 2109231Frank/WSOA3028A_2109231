be3e9f4bf35ee054c2727adefd0e98fcd88c005f
2109231Frank <2109231@students.wits.ac.za>
2021-06-15 14:38:30 +0200

    Mobile navbar (java animation and css)

--- site.js
+++ site.js
@@ -1,5 +1,29 @@
 
-const navigationLinks = document.querySelector(".nav a")
+//Mobile navbar animation
+const navigationAnimation = () => 
+{
+    const threestripe = document.querySelector('.threestripe');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
+        threestripe.addEventListener('click', () => 
+        {
+            nav.classList.toggle('navigationAnim');
 
-navigationLinks.forEach(elem => elem.addEventListener("click", navigationLinkClick));
+            navLinks.forEach((link, index) => 
+            {
+                if(link.style.animation)
+                {
+                    link.style.animation = '';
+                }
+                else
+                {
+                    link.style.animation = `nav-linkAnim 0.7s ease forwards ${index/ 7 + 0.5}s`;
+                }
+            });
+            
+            threestripe.classList.toggle('.toggle')
+        
+        });
+}
+navigationAnimation();
