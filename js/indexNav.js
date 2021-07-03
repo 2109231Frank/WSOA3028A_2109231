54171b82101dd0a71e58994915c78bf67f0bf72e
2109231Frank <2109231@students.wits.ac.za>
2021-07-03 18:25:14 +0200

    minor fixes

--- js/indexNav.js
+++ js/indexNav.js
@@ -31,37 +31,3 @@ for(let item of menuItems) {
 document.addEventListener('DOMContentLoaded', () => initialiseMenu());
 
 
-//Mobile and tablet navbar animation
-const navigationAnimation = () => 
-{
-    const threestripe = document.querySelector('.threestripe');
-    const nav = document.querySelector('nav');
-    const navLinks = document.querySelector('nav li');
-
-
-        threestripe.addEventListener('click', () => 
-        {
-            nav.classList.toggle('navigationAnim');
-                
-            navLinks.forEach((link, index) => 
-            {
-               
-            
-
-                if(link.style.animation)
-                {
-                    link.style.animation = '';
-                   
-                        
-                }
-                else
-                {
-                    link.style.animation = `nav-linkAnim 0.7s ease forwards ${index/ 7 + 0.5}s`;
-                    
-                }
-            });
-        });
-
-       
-}
-navigationAnimation();
--- js/pagesNav.js
+++ js/pagesNav.js
@@ -30,37 +30,3 @@ for(let item of menuItems) {
 document.addEventListener('DOMContentLoaded', () => initialiseMenu());
 
 
-//Mobile and tablet navbar animation
-const navigationAnimation = () => 
-{
-    const threestripe = document.querySelector('.threestripe');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-
-        threestripe.addEventListener('click', () => 
-        {
-            nav.classList.toggle('navigationAnim');
-                
-            navLinks.forEach((link, index) => 
-            {
-               
-            
-
-                if(link.style.animation)
-                {
-                    link.style.animation = '';
-                   
-                        
-                }
-                else
-                {
-                    link.style.animation = `nav-linkAnim 0.7s ease forwards ${index/ 7 + 0.5}s`;
-                    
-                }
-            });
-        });
-
-       
-}
-navigationAnimation();
