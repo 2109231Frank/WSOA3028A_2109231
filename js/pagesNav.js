d254c86a6789be771349df1be6eade93d06a07c0
2109231Frank <2109231@students.wits.ac.za>
2021-07-03 14:38:07 +0200

    Navigation transferred to js

---
+++ js/pagesNav.js
@@ -0,0 +1,67 @@
+const menuItems = [
+    {title: 'HOME', link: '../index.html'},
+    {title: 'PROJECTS', link: 'Projects.html'},
+    {title: 'PERSONAL', link: 'Mikes-Personal-Blogs.html'},
+    {title: 'INTERACTIVE', link: 'Interactive-Media-Blogs.html'},
+    {title: 'CONTACT', link: 'Contact.html'},
+    {title: 'WORK', link: 'Work.html'},
+    {title: 'TEMPLATE', link: 'WireFrame.html'},
+
+]
+
+const initialiseMenu = () => {
+
+const nav = document.querySelector('nav');
+
+const ul = document.createElement('ul');
+
+for(let item of menuItems) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+        a.innerText = item.title;
+        a.href = item.link;
+            li.appendChild(a);
+
+        ul.appendChild(li);
+}
+
+    nav.appendChild(ul);
+};
+
+document.addEventListener('DOMContentLoaded', () => initialiseMenu());
+
+
+//Mobile and tablet navbar animation
+const navigationAnimation = () => 
+{
+    const threestripe = document.querySelector('.threestripe');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+        threestripe.addEventListener('click', () => 
+        {
+            nav.classList.toggle('navigationAnim');
+                
+            navLinks.forEach((link, index) => 
+            {
+               
+            
+
+                if(link.style.animation)
+                {
+                    link.style.animation = '';
+                   
+                        
+                }
+                else
+                {
+                    link.style.animation = `nav-linkAnim 0.7s ease forwards ${index/ 7 + 0.5}s`;
+                    
+                }
+            });
+        });
+
+       
+}
+navigationAnimation();
